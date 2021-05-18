a615a8f3087a474015182477d8ed9ccc8fb8ac61
JohnKrim <reamonn96@gmail.com>
2021-05-18 17:33:50 +0300

    Lesson work end

---
+++ PersonalQuiz/View/ResultViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Kalabishka Ivan on 17.05.2021.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+
+
+
+}
